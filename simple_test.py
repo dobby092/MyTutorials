2c30616c42b14b542b70cd9e01e4ff4cb379cef5
Dami <damian092@gmail.com>
2023-01-27 18:28:32 -0300

    Added intro

--- simple_test.py
+++ simple_test.py
@@ -3,6 +3,7 @@ def higher():
     """
     Input two numbers and returns the higher one.
     """
+    print("I'll have you pick two numbers and I'll show you the greatest one")
     a, b = 'a', 'b'
     while type(a) != int:
         try: a = int(input('First number: '))
@@ -10,13 +11,12 @@ def higher():
     while type(b) != int:
         try: b = int(input('Second number: '))
         except Exception as e: print('Please, try again.')
+
     if a >= b:
         c = a
     elif a < b:
         c = b
-    else:
-        print("Something went wrong. It's probably your fault, but let me help you")
-        exit
+
     return print(c)
 
 higher()
